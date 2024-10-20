7dc40c55902426065a78add7971b8d2a8c75e8ff
DamianStrz <damians.praca@gmail.com>
2024-10-20 10:17:28 +0200

    CSSBasline, Container and RouterProvider moved to separate file named App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import router from './routes/routes.tsx';
+import App from './components/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CssBaseline />
-    <Container maxWidth={false} sx={{maxWidth: "2560px"}}>
-      <RouterProvider router={router} />
-    </Container>
+    <App />
   </StrictMode>,
 );
